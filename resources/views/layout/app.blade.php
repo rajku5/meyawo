94d7192e65f11ced8295111d54a5cf2387fa428a
raj kumar <rajku59421@gmail.com>
2023-07-03 18:43:54 +0530

    first commit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Start your development with Meyawo landing page.">
+        <meta name="author" content="Devcrud">
+        <title>Meyawo Landing page | Free Bootstrap 4.3.x landing page</title>
+        <!-- font icons -->
+        <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+        <!-- Bootstrap + Meyawo main styles -->
+        <link rel="stylesheet" href="assets/css/meyawo.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
